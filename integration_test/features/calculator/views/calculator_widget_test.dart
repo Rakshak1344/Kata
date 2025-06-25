b00f83df3d3b8383952000ad51afbce57adb63ca
Rakshak1344 <dev.rakshith14@gmail.com>
2025-06-25 15:14:24 +0530

    chore: fix typo, and grouped

--- integration_test/features/calculator/views/calculator_widget_test.dart
+++ integration_test/features/calculator/views/calculator_widget_test.dart
@@ -8,93 +8,114 @@ import 'package:kata/utils/keys.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('UI test on calculator page', (WidgetTester tester) async {
-    await tester.pumpWidget(const KataApp());
-    await tester.pumpAndSettle();
+  group("Calculator Page UI Tests", () {
+    testWidgets('should result 3 when given \'1,2\'', (
+      WidgetTester tester,
+    ) async {
+      await tester.pumpWidget(const KataApp());
+      await tester.pumpAndSettle();
 
-    await tester.enterText(find.byKey(K.calculator.textField), '1,2');
-    await tester.pumpAndSettle();
+      await tester.enterText(find.byKey(K.calculator.textField), '1,2');
+      await tester.pumpAndSettle();
 
-    await tester.tap(find.byKey(K.calculator.calculateButton));
-    await tester.pumpAndSettle();
+      await tester.tap(find.byKey(K.calculator.calculateButton));
+      await tester.pumpAndSettle();
 
-    expect(find.byKey(K.calculator.calculationResultText), findsOneWidget);
-    expect(find.textContaining('Result: 3'), findsOneWidget);
-  });
+      expect(find.byKey(K.calculator.calculationResultText), findsOneWidget);
+      expect(find.textContaining('Result: 3'), findsOneWidget);
+    });
 
-  testWidgets('Calculator should throw error on negative input', (
-    WidgetTester tester,
-  ) async {
-    await tester.pumpWidget(const KataApp());
-    await tester.pumpAndSettle();
+    testWidgets('should result 0 when given string is empty', (
+      WidgetTester tester,
+    ) async {
+      await tester.pumpWidget(const KataApp());
+      await tester.pumpAndSettle();
 
-    await tester.enterText(
-      find.byKey(K.calculator.textField),
-      '///(%#),10,-200',
+      await tester.enterText(find.byKey(K.calculator.textField), '');
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(K.calculator.calculateButton));
+      await tester.pumpAndSettle();
+
+      expect(find.byKey(K.calculator.calculationResultText), findsOneWidget);
+      expect(find.textContaining('Result: 0'), findsOneWidget);
+    });
+
+    testWidgets(
+      'Should result error when single negative input given in a string',
+      (WidgetTester tester) async {
+        await tester.pumpWidget(const KataApp());
+        await tester.pumpAndSettle();
+
+        await tester.enterText(
+          find.byKey(K.calculator.textField),
+          '///(%#),10,-200',
+        );
+        await tester.pumpAndSettle();
+
+        await tester.tap(find.byKey(K.calculator.calculateButton));
+        await tester.pumpAndSettle();
+
+        expect(find.byKey(K.calculator.calculationResultText), findsOneWidget);
+        expect(
+          find.textContaining('Negative number is not allowed: -200'),
+          findsOneWidget,
+        );
+      },
     );
-    await tester.pumpAndSettle();
 
-    await tester.tap(find.byKey(K.calculator.calculateButton));
-    await tester.pumpAndSettle();
+    testWidgets('Should result error when multiple negative input given', (
+      WidgetTester tester,
+    ) async {
+      await tester.pumpWidget(const KataApp());
+      await tester.pumpAndSettle();
 
-    expect(find.byKey(K.calculator.calculationResultText), findsOneWidget);
-    expect(
-      find.textContaining('Negative number is not allowed: -200'),
-      findsOneWidget,
-    );
-  });
+      await tester.enterText(
+        find.byKey(K.calculator.textField),
+        '///(%#),10,-5}-90',
+      );
+      await tester.pumpAndSettle();
 
-  testWidgets('Calculator should throw error on multiple negative input', (
-    WidgetTester tester,
-  ) async {
-    await tester.pumpWidget(const KataApp());
-    await tester.pumpAndSettle();
+      await tester.tap(find.byKey(K.calculator.calculateButton));
+      await tester.pumpAndSettle();
 
-    await tester.enterText(
-      find.byKey(K.calculator.textField),
-      '///(%#),10,-5}-90',
-    );
-    await tester.pumpAndSettle();
+      expect(find.byKey(K.calculator.calculationResultText), findsOneWidget);
+      expect(
+        find.textContaining('Negative numbers are not allowed: -5, -90'),
+        findsOneWidget,
+      );
+    });
 
-    await tester.tap(find.byKey(K.calculator.calculateButton));
-    await tester.pumpAndSettle();
+    testWidgets('Calculator delimiter(s) button test', (
+      WidgetTester tester,
+    ) async {
+      await tester.pumpWidget(const KataApp());
+      await tester.pumpAndSettle();
 
-    expect(find.byKey(K.calculator.calculationResultText), findsOneWidget);
-    expect(
-      find.textContaining('Negative numbers are not allowed: -5, -90'),
-      findsOneWidget,
-    );
-  });
+      await tester.enterText(find.byKey(K.calculator.textField), '5');
+      await tester.pumpAndSettle();
 
-  testWidgets('Calculator delimiter(s) button test', (
-    WidgetTester tester,
-  ) async {
-    await tester.pumpWidget(const KataApp());
-    await tester.pumpAndSettle();
+      await tester.tap(
+        find.byKey(K.calculator.delimiterButton(Consts.delimiters[0])),
+      );
+      await tester.pumpAndSettle();
+      await tester.tap(
+        find.byKey(K.calculator.delimiterButton(Consts.delimiters[1])),
+      );
+      await tester.pumpAndSettle();
+      await tester.tap(
+        find.byKey(K.calculator.delimiterButton(Consts.delimiters[2])),
+      );
+      await tester.pumpAndSettle();
+      await tester.tap(
+        find.byKey(K.calculator.delimiterButton(Consts.delimiters[3])),
+      );
+      await tester.pumpAndSettle();
 
-    await tester.enterText(find.byKey(K.calculator.textField), '5');
-    await tester.pumpAndSettle();
-
-    await tester.tap(
-      find.byKey(K.calculator.delimiterButton(Consts.delimiters[0])),
-    );
-    await tester.pumpAndSettle();
-    await tester.tap(
-      find.byKey(K.calculator.delimiterButton(Consts.delimiters[1])),
-    );
-    await tester.pumpAndSettle();
-    await tester.tap(
-      find.byKey(K.calculator.delimiterButton(Consts.delimiters[2])),
-    );
-    await tester.pumpAndSettle();
-    await tester.tap(
-      find.byKey(K.calculator.delimiterButton(Consts.delimiters[3])),
-    );
-    await tester.pumpAndSettle();
-
-    final textField = tester.widget<TextField>(
-      find.byKey(K.calculator.textField),
-    );
-    expect(textField.controller?.text, '5//[**]\\n/t');
+      final textField = tester.widget<TextField>(
+        find.byKey(K.calculator.textField),
+      );
+      expect(textField.controller?.text, '5//[**]\\n/t');
+    });
   });
 }
